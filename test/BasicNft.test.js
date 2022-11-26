dbd640ab3e90af5e3661ad35039334e0c8e069a1
ermz <eramirez4369@gmail.com>
2022-11-26 13:26:13 -0500

    Fixed testing structure and added deployment testing

--- test/BasicNft.test.js
+++ test/BasicNft.test.js
@@ -1,28 +1,36 @@
 const { ethers } = require('hardhat')
 const { use, expect } = require('chai')
+const { developmentChains } = require('../helper-hardhat-config')
 // const { solidity } = require('ethereum-waffle')
 
 // use(solidity)
 
-describe('BasicNft deployment', function () {
+!developmentChains.includes(network.name)
+    ? describe.skip
+    :describe('BasicNft deployment', function () {
 
-    let BasicNFT
-    beforeEach(async () => {
-        [deployer, user1, user2] = await ethers.getSigners();
-        const BasicNFT_contract = await ethers.getContractFactory('BasicNft');
-        BasicNFT = await BasicNFT_contract.deploy();
-    })
+        let BasicNFT
+        beforeEach(async () => {
+            [deployer, user1, user2] = await ethers.getSigners();
+            const BasicNFT_contract = await ethers.getContractFactory('BasicNft');
+            BasicNFT = await BasicNFT_contract.deploy();
+        })
 
-    it("Should mint an NFT", async () => {
-        expect(await BasicNFT.getTokenCounter()).to.equal(0);
-        await BasicNFT.mintNft();
-        expect(await BasicNFT.getTokenCounter()).to.equal(1);
-    })
+        it("Should have deployed correctly", async () => {
+            expect(await BasicNFT.name()).to.equal("Dogie");
+            expect(await BasicNFT.symbol()).to.equal("DOG");
+        })
 
-    it("Should increase balance of owner", async () => {
-        expect(await BasicNFT.balanceOf(deployer.address)).to.equal(0);
-        await BasicNFT.mintNft();
-        expect(await BasicNFT.balanceOf(deployer.address)).to.equal(1);
-        expect(await BasicNFT.ownerOf(1)).to.equal(deployer.address);
+        it("Should mint an NFT", async () => {
+            expect(await BasicNFT.getTokenCounter()).to.equal(0);
+            await BasicNFT.mintNft();
+            expect(await BasicNFT.getTokenCounter()).to.equal(1);
+        })
+
+        it("Should increase balance of owner", async () => {
+            expect(await BasicNFT.balanceOf(deployer.address)).to.equal(0);
+            await BasicNFT.mintNft();
+            expect(await BasicNFT.balanceOf(deployer.address)).to.equal(1);
+            expect(await BasicNFT.ownerOf(1)).to.equal(deployer.address);
+        })
     })
-})
