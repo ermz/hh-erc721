4a70e69615014d08c3474f4f21aea4aaff48e132
ermz <eramirez4369@gmail.com>
2022-12-13 23:55:31 -0500

    Added base64 code and minor changes

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
     // mint
@@ -10,10 +11,20 @@ contract DynamicSvgNft is ERC721 {
     // some logic to say "Show  X image" or "Show Y Image"
 
     uint256 private s_tokenCounter;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,"
 
-    constructor() ERC721("Dynamic SVG NFT", "DSN") {}
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+
+    }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
     }
 }
